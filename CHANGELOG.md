f5b2aafe314f68cef4ce79c91a0103373f3059a1
Thorben Schröder <stillepost@gmail.com>
2013-02-20 22:54:40 +0100

    Bumps to 0.0.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-# 0.0.4 WIP
+# 0.0.4
 
 * Fixes return values of setters
 
--- lib/cache-backend-iron-cache/version.rb
+++ lib/cache-backend-iron-cache/version.rb
@@ -1,7 +1,7 @@
 module Cache
   module Backend
     class IronCache
-      VERSION = "0.0.3"
+      VERSION = "0.0.4"
     end
   end
 end
